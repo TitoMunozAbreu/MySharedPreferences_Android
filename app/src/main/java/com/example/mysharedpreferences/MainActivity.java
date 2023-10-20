c53a2a9aa0c08f4ab5115c448766b54ac5156bc1
titomunoz <munoztitojose@gmail.com>
2023-10-20 10:55:23 +0200

    Tarea: Agenda de contacto

--- app/src/main/java/com/example/mysharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/mysharedpreferences/MainActivity.java
@@ -11,20 +11,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nombre;
-    private EditText email;
-    private EditText movil;
-    private Button btnGuardar;
-    private Button btnBuscar;
-    //constantes
-    private static final String N = "nombre";
-    private static final String E = "email";
-    private static final String M = "movil";
-
-    private String nombreGuardado;
-    private String emailGuardado;
-    private String movilGuardado;
-
+    private EditText nombre, email, movil;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,55 +23,45 @@ public class MainActivity extends AppCompatActivity {
         this.nombre = findViewById(R.id.nombre);
         this.email = findViewById(R.id.email);
         this.movil = findViewById(R.id.movil);
-        this.btnBuscar = findViewById(R.id.btnBuscar);
-        this.btnGuardar = findViewById(R.id.btnGuardar);
-
     }
-
-
 
     public void guardar(View view){
         //instanciar el sharedPreferences
-        SharedPreferences sharedPreferences = getSharedPreferences("SharePreferences", MODE_PRIVATE);
+        this.sharedPreferences = getSharedPreferences("SharePreferences", MODE_PRIVATE);
         //instanciar el editor
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        //establecer las variables que guardaran los datos
-        editor.putString(N, nombre.getText().toString());
-        editor.putString(E, email.getText().toString());
-        editor.putString(M, movil.getText().toString());
-
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        //almacenar los datos ingresados en el editor
+        editor.putString("nombre", nombre.getText().toString());
+        editor.putString("email", email.getText().toString());
+        editor.putString("movil", movil.getText().toString());
         //notificar que ha sido guardado
         Toast.makeText(this, "Datos almacenados", Toast.LENGTH_SHORT).show();
-        editor.apply();
-
-    }
-
-    public void cargarDatos(){
-        //instanciar el sharedPreferences
-        SharedPreferences sharedPreferences = getSharedPreferences("SharePreferences", MODE_PRIVATE);
-
-        //crear las variables que recibiran los datos que han sido guardados
-        this.nombreGuardado = sharedPreferences.getString(N,"");
-        this.emailGuardado = sharedPreferences.getString(E,"");
-        this.movilGuardado = sharedPreferences.getString(M,"");
-
+        //guardar cambios realizados
+        editor.commit();
+        finish();
     }
 
     private void actualizarVista() {
-        this.nombre.setText(this.nombreGuardado);
-        this.email.setText(this.emailGuardado);
-        this.movil.setText(this.movilGuardado);
+        //actualizar los campos con los valores del alamcenados en editor
+        this.nombre.setText(this.sharedPreferences.getString("nombre",""));
+        this.email.setText(this.sharedPreferences.getString("email",""));
+        this.movil.setText(this.sharedPreferences.getString("movil",""));
     }
 
     public void buscar(View view){
+        //variable contiene el nombre guardado en el editor
+        String nombreAlamcenado = this.sharedPreferences.getString("nombre","");
+        //variable contiene el nombre ingresado a buscar
         String nombreABuscar = this.nombre.getText().toString();
-        cargarDatos();
 
-        if(this.nombreGuardado.equals(nombreABuscar)){
+        //comprobar que el nombreABuscar se encuentre en el Editor
+        if(nombreABuscar.equals(nombreAlamcenado)){
             actualizarVista();
         }else {
             Toast.makeText(this,"NO existen datos asociados a ese nombre", Toast.LENGTH_SHORT).show();
+
         }
+
     }
 
 
